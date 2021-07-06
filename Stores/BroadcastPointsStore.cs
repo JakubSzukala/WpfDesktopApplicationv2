964c2cdf4d58727e3578e2a9906542f268d56ce5
JakubSzukala <burski09@outlook.com>
2021-07-06 15:58:43 +0200

    Created mediator class for broadcasting data to plot objects.

---
+++ Stores/BroadcastPointsStore.cs
@@ -0,0 +1,17 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfDesktopApplicationv2.Stores
+{
+    public class BroadcastPointsStore
+    {
+        public event EventHandler<Dictionary<string, DataPoint>> PointsBroadcasted;
+
+        public void OnPointsBroadcasted(Dictionary<string, DataPoint> dictionary)
+        {
+            PointsBroadcasted?.Invoke(this, dictionary);
+        }
+    }
+}
