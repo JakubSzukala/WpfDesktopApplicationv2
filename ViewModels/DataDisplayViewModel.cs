74635a4bfd67ac13b0f456b12c108ebafa3b725e
JakubSzukala <burski09@outlook.com>
2021-07-06 15:59:29 +0200

    Added data aquisition, formatting and broadcasting to charts.

--- ViewModels/DataDisplayViewModel.cs
+++ ViewModels/DataDisplayViewModel.cs
@@ -1,30 +1,101 @@
 ﻿using Newtonsoft.Json.Linq;
+using OxyPlot;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Timers;
 using WpfDesktopApplicationv2.Models;
+using WpfDesktopApplicationv2.Stores;
 
 namespace WpfDesktopApplicationv2.ViewModels
 {
     class DataDisplayViewModel
     {
         //properties
-        public ObservableCollection<MeasurementViewModel> measurements { get; set; }
+        public ObservableCollection<MeasurementViewModel> MeasurementsVM { get; set; } // src of problems? Will observable collection trigger?
+        public float SamplingTime { get; set; }
+        public DataPlotViewModel ActivePlot { get; set; }
 
-        
+
         //fields
-        private readonly ServerIoTmock _server;
+        private readonly DataConverterModel _converter;
+        private Dictionary<string, DataPoint> BroadcastDataPoints;
+        private LinearAxesModel Axes;
+
+        // fields - charts
+        public DataPlotViewModel TemperatureChart { get; set; }
+        public DataPlotViewModel PressureChart { get; set; }
+        public DataPlotViewModel HumidityChart{ get; set; }
+
+        // Stores
+        private BroadcastPointsStore broadcastPointsStore;
+
+        private Timer RequestTimer;
 
 
         public DataDisplayViewModel()
         {
-            _server = new ServerIoTmock();
+            // data aquisition initialization
+            _converter = new DataConverterModel();
+            MeasurementsVM = _converter.RequestViewModelsFromServer();
+            BroadcastDataPoints = _converter.RequestDataPointsFromServer(1F);
+
+            // model containing models of available axes configuration
+            Axes = new LinearAxesModel();
+
+            //Store initialization
+            broadcastPointsStore = new BroadcastPointsStore();
+
+            // charts initialization
+            TemperatureChart = new DataPlotViewModel("Temperature", Axes.Temperature, broadcastPointsStore);
+            PressureChart = new DataPlotViewModel("Pressure", Axes.Pressure, broadcastPointsStore);
+            HumidityChart = new DataPlotViewModel("Humidity", Axes.Humidity, broadcastPointsStore);
+
+            ActivePlot = TemperatureChart;
+
+            // temporary
+            SamplingTime = 1F;
+
+            SetTimer();
         }
 
-        private void GetData()
+        /// <summary>
+        /// Function that sets data update routine for application.
+        /// </summary>
+        private void SetTimer()
         {
+            RequestTimer = new Timer(SamplingTime*1000);
             
+            // Update dictionary with points 
+            RequestTimer.Elapsed += UpdateBroadcastDataPointsWrapper;
+
+            // broadcast this dictionary to listening charts 
+            RequestTimer.Elapsed += BroadcastDataPointsWrapper;
+
+
+            RequestTimer.AutoReset = true;
+            RequestTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Function that wraps RequestDataPointsFromServer of _converter to match delegate of Elapsed event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UpdateBroadcastDataPointsWrapper(object sender, EventArgs e)
+        {
+            BroadcastDataPoints = _converter.RequestDataPointsFromServer(1F);
+        }
+
+        /// <summary>
+        /// Function that will publish the data in form of event to which charts will subscribe
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BroadcastDataPointsWrapper(object sender, EventArgs e)
+        {
+            broadcastPointsStore.OnPointsBroadcasted(BroadcastDataPoints);
         }
     }
 }
---
+++ ViewModels/DataPlotViewModel.cs
@@ -0,0 +1,88 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Threading;
+using WpfDesktopApplicationv2.Stores;
+
+namespace WpfDesktopApplicationv2.ViewModels
+{
+    class DataPlotViewModel:INotifyPropertyChanged
+    {
+        // properties
+        public PlotModel DataPlotModel { get; set; }
+
+        // fields
+        private string _identifier;
+        BroadcastPointsStore _broadcast;
+
+        // events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DataPlotViewModel(string identifier, LinearAxis axis, BroadcastPointsStore broadcast)
+        {
+            // assign identifier
+            _identifier = identifier;
+
+            // initialize plot on the basis of this identifier
+            DataPlotModel = new PlotModel() { Title = _identifier };
+            DataPlotModel.Axes.Add(axis);
+            DataPlotModel.Axes.Add(new LinearAxis()
+            {
+                Position = AxisPosition.Bottom,
+                Minimum = 0,
+                Maximum = 10,
+                Key = "Horizontal",
+                Unit = "sec",
+                Title = "Time"
+            });
+
+            // add empty series for now
+            DataPlotModel.Series.Add(new LineSeries() { Title = _identifier + " data series", Color = OxyColor.Parse("#FFFF0000") });
+
+            // initialize broadcast on which plot will receive dictionary with points
+            _broadcast = broadcast;
+
+            // subscribe to event
+            _broadcast.PointsBroadcasted += Update;
+        }
+
+        public void Update(object sender, Dictionary<string, DataPoint> dictionary)// object sender, event args
+        {
+            Debug.WriteLine("Updating a plot...");
+            Debug.WriteLine("Identifier of this plot is: " + _identifier);
+            Debug.WriteLine("Point is: " + PickMeasurement(dictionary));
+
+            LineSeries lineSeries = DataPlotModel.Series[0] as LineSeries;
+            lineSeries.Points.Add(PickMeasurement(dictionary));
+
+            DataPlotModel.InvalidatePlot(true);
+            OnPropertyChanged("DataPlotModel"); // ? 
+        }
+
+        /// <summary>
+        /// Pick a measurement from broadcasted dictionary of points for different measurements.
+        /// Measurement is picked based on identifier.
+        /// </summary>
+        private DataPoint PickMeasurement(Dictionary<string, DataPoint> dictionary)
+        {
+            foreach(var m in dictionary)
+            {
+                if(m.Key == _identifier)
+                {
+                    return m.Value;
+                }
+            }
+            return new DataPoint(0, 0);
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
