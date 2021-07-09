5b4eeeb6d576431e5457911ba76a53099f2149f5
JakubSzukala <burski09@outlook.com>
2021-07-09 14:54:02 +0200

    Cleaned up a little ServerIoT class.

--- Models/ServerIoT.cs
+++ Models/ServerIoT.cs
@@ -24,9 +24,7 @@ namespace WpfDesktopApplicationv2.Models
             _error = err;
         }
 
-        /**
-         * @brief obtaining the address of the data file from IoT server IP.
-         */
+        
         private string GetFileUrl()
         {
             return "http://" + ip + "/IOT_server/sensors_via_deamon.php?id=env";
@@ -41,19 +39,11 @@ namespace WpfDesktopApplicationv2.Models
             return "http://" + ip + "/IOT_server/sensors_via_deamon.php?id=ori";
         }
 
-        private string GetFileUrlJoy()
-        {
-            return "http://" + ip + "/server/joystick_via_deamon.php?id=get";
-        }
-
-        private string GetFileUrlJoyClear()
-        {
-            return "http://" + ip + "/server/joystick_via_deamon.php?id=rst";
-        }
+        
 
 
         /// <summary>
-        /// 
+        /// Gets the data from server about measurements in form of json string.
         /// </summary>
         /// <returns></returns>
         public async Task<string> GETwithClient()
@@ -100,29 +90,15 @@ namespace WpfDesktopApplicationv2.Models
             return responseText;
         }
 
-        public async Task<string> GETwithClientJoy()
-        {
-            HttpClient httpClient = new HttpClient();
+        
 
-            string responseText = await httpClient.GetStringAsync(GetFileUrlJoy());
+        
 
-            httpClient.Dispose();
-            return responseText;
-        }
-
-        public async Task<string> GETwithClientJoyClear()
-        {
-            HttpClient httpClient = new HttpClient();
-
-            string responseText = await httpClient.GetStringAsync(GetFileUrlJoyClear());
-
-            httpClient.Dispose();
-            return responseText;
-        }
-
-        /**
-          * @brief HTTP POST request using HttpClient
-         */
+        /// <summary>
+        /// Post data about led matrix state with client
+        /// </summary>
+        /// <param name="data">Data in necessary form that will be sent to server.</param>
+        /// <returns></returns>
         public async Task<string> POSTwithClient(Dictionary<string, List<int>> data)
         {
             string responseText = null;
@@ -155,262 +131,6 @@ namespace WpfDesktopApplicationv2.Models
             return responseText;
         }
 
-        public async Task<string> POSTwithClientRPY()
-        {
-            string responseText = null;
-
-            try
-            {
-                using (HttpClient client = new HttpClient())
-                {
-                    // POST request data
-                    var requestDataCollection = new List<KeyValuePair<string, string>>();
-                    requestDataCollection.Add(new KeyValuePair<string, string>("filename", "sensors_via_deamon.php"));
-                    var requestData = new FormUrlEncodedContent(requestDataCollection);
-                    // Sent POST request
-                    var result = await client.PostAsync(GetFileUrlAngles(), requestData);
-                    // Read response content
-                    responseText = await result.Content.ReadAsStringAsync();
-
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("NETWORK ERROR");
-                Debug.WriteLine(e);
-            }
-
-            return responseText;
-        }
-
-        public async Task<string> POSTwithClientJoy()
-        {
-            string responseText = null;
-
-            try
-            {
-                using (HttpClient client = new HttpClient())
-                {
-                    // POST request data
-                    var requestDataCollection = new List<KeyValuePair<string, string>>();
-                    requestDataCollection.Add(new KeyValuePair<string, string>("filename", "joystick_via_deamon.php"));
-                    var requestData = new FormUrlEncodedContent(requestDataCollection);
-                    // Sent POST request
-                    var result = await client.PostAsync(GetFileUrlJoy(), requestData);
-                    // Read response content
-                    responseText = await result.Content.ReadAsStringAsync();
-
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("NETWORK ERROR");
-                Debug.WriteLine(e);
-            }
-
-            return responseText;
-        }
-
-        /**
-          * @brief HTTP GET request using HttpWebRequest
-          */
-        public async Task<string> GETwithRequest()
-        {
-            string responseText = null;
-
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GetFileUrl());
-
-                request.Method = "GET";
-
-                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    responseText = await reader.ReadToEndAsync();
-                }
-
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("NETWORK ERROR");
-                Debug.WriteLine(e);
-            }
-
-            return responseText;
-        }
-
-        public async Task<string> GETwithRequestRPY()
-        {
-            string responseText = null;
-
-            try
-            {
-
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GetFileUrlAngles());
-
-                request.Method = "GET";
-
-                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    responseText = await reader.ReadToEndAsync();
-                }
-
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("NETWORK ERROR");
-                Debug.WriteLine(e);
-            }
-
-            return responseText;
-        }
-
-        public async Task<string> GETwithRequestJoy()
-        {
-            string responseText = null;
-
-            try
-            {
-
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GetFileUrlJoy());
-
-                request.Method = "GET";
-
-                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    responseText = await reader.ReadToEndAsync();
-                }
-
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("NETWORK ERROR");
-                Debug.WriteLine(e);
-            }
-
-            return responseText;
-        }
-
-
-        /**
-          * @brief HTTP POST request using HttpWebRequest
-          */
-        public async Task<string> POSTwithRequest()
-        {
-            string responseText = null;
-
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GetFileUrl());
-
-                // POST Request data 
-                var requestData = "filename=sensors_via_deamon.php";
-                byte[] byteArray = Encoding.UTF8.GetBytes(requestData);
-                // POST Request configuration
-                request.Method = "POST";
-                request.ContentType = "application/x-www-form-urlencoded";
-                request.ContentLength = byteArray.Length;
-                // Wrire data to request stream
-                Stream dataStream = request.GetRequestStream();
-                dataStream.Write(byteArray, 0, byteArray.Length);
-                dataStream.Close();
-
-                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    responseText = await reader.ReadToEndAsync();
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("NETWORK ERROR");
-                Debug.WriteLine(e);
-            }
-
-            return responseText;
-        }
-
-
-        public async Task<string> POSTwithRequestRPY()
-        {
-            string responseText = null;
-
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GetFileUrlAngles());
-
-                // POST Request data 
-
-                var requestData = "filename=sensors_via_deamon.php";
-                byte[] byteArray = Encoding.UTF8.GetBytes(requestData);
-                // POST Request configuration
-                request.Method = "POST";
-                request.ContentType = "application/x-www-form-urlencoded";
-                request.ContentLength = byteArray.Length;
-                // Wrire data to request stream
-                Stream dataStream = request.GetRequestStream();
-                dataStream.Write(byteArray, 0, byteArray.Length);
-                dataStream.Close();
-
-                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    responseText = await reader.ReadToEndAsync();
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("NETWORK ERROR");
-                Debug.WriteLine(e);
-            }
-
-            return responseText;
-        }
-
-        public async Task<string> POSTwithRequestJoy()
-        {
-            string responseText = null;
-
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GetFileUrlJoy());
-
-                // POST Request data 
-
-                var requestData = "filename=joystick_via_deamon.php";
-                byte[] byteArray = Encoding.UTF8.GetBytes(requestData);
-                // POST Request configuration
-                request.Method = "POST";
-                request.ContentType = "application/x-www-form-urlencoded";
-                request.ContentLength = byteArray.Length;
-                // Wrire data to request stream
-                Stream dataStream = request.GetRequestStream();
-                dataStream.Write(byteArray, 0, byteArray.Length);
-                dataStream.Close();
-
-                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    responseText = await reader.ReadToEndAsync();
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("NETWORK ERROR");
-                Debug.WriteLine(e);
-            }
-
-            return responseText;
-        }
-
+        
     }
 }
